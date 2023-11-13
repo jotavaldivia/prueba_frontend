92a2d3fc63a018c1006c7d8ce921c7041332168a
juan valdebenito <valdebenbitovaldiviajuan@gmail.com>
2023-11-13 13:01:07 -0300

    add bread component

---
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './BreadCrumbs.module.css'
+
+const BreadCrumbs = () => {
+  return (
+    <div className={s.container}>
+        <p className={s.Bread}>INICIO / PROTEÍNAS / WHEY PROTEIN</p>
+    </div>
+  )
+}
+
+export default BreadCrumbs
--- src/pages/Pdp/Pdp.jsx
+++ src/pages/Pdp/Pdp.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Bar from '../../components/Bar/Bar.jsx'
 import Navbar from '../../components/NavBar/Navbar.jsx'
+import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs.jsx'
+import s from './Pdp.module.css'
  const Pdp = () => {
   return (
     <div>
         <Bar />
         <Navbar />
+        <div className={s.container}>
+
+        <BreadCrumbs />
+        </div>
     </div>
   )
 }
